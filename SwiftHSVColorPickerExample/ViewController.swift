0723c21f824ca7ea00fa7d8871a861e34cb4b710
Martin Imobersteg <martin.imobersteg@gmail.com>
2018-10-26 15:18:48 +0200

    Swift lint

--- SwiftHSVColorPickerExample/ViewController.swift
+++ SwiftHSVColorPickerExample/ViewController.swift
@@ -9,13 +9,13 @@ import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     // Init ColorPicker with yellow
     var selectedColor: UIColor = UIColor.white
-    
+
     // IBOutlet for the ColorPicker
     @IBOutlet var colorPicker: SwiftHSVColorPicker!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,20 +23,20 @@ class ViewController: UIViewController {
         // Setup Color Picker
         colorPicker.setViewColor(selectedColor)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func getSelectedColor(_ sender: UIButton) {
         // Get the selected color from the Color Picker.
         let selectedColor = colorPicker.color
-        
+
         print(selectedColor!)
     }
 }
